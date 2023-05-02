eeafa2ed2a82d61e72c4deeb5c6f37e510fd369f
Joshua Graber <joshua.d.graber@gmail.com>
2023-05-02 08:24:36 -0400

    Initial commit: Add configs, Add packages, Add src and public directories

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'styles/app.scss';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a React / Typescript sandbox created by Joshua D. Graber</p>
+    </>
+  );
+};
+
+export default App;
